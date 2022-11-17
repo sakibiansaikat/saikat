e60fe5635f9dc4f142650fbd575521a631ec4476
Saikat khan <118449280+sakibiansaikat@users.noreply.github.com>
2022-11-17 20:08:24 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const menuIcon = document.getElementById("menu-icon2");
+const menu = document.getElementById("menu-icon");
+menuIcon ?.addEventListener("click",()=>{
+    if(menu?.className =="hidden"){
+        menu.classList.remove("hidden");
+    }else{
+        menu.classList.add("hidden");
+    }
+})
